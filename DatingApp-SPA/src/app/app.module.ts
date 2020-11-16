efbff430b5ac97631846fb3ce109341919af0819
Tendai mudadi <tendaimudadi@gmail.com>
2020-11-16 21:12:44 +0200

    Routing

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -5,6 +5,8 @@ import {FormsModule} from '@angular/forms';
 import {AuthService} from './_services/auth.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from '../app/routes';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -15,13 +17,19 @@ import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { AlertifyService } from './_services/alertify.service';
 import {ToastrModule} from 'ngx-toastr';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      MemberListComponent,
+      ListsComponent,
+      MessagesComponent
    ],
    imports: [
       BrowserAnimationsModule,
@@ -29,6 +37,7 @@ import {ToastrModule} from 'ngx-toastr';
       BrowserModule,
       HttpClientModule,
       FormsModule,
+      RouterModule.forRoot(appRoutes),
       ToastrModule.forRoot({
         positionClass: 'toast-bottom-right'
       })
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
 import {ToastrService} from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -12,28 +13,37 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService,
+  constructor(private router: Router,
+              public authService: AuthService, private alertify: AlertifyService,
               private toastr: ToastrService) { }
 
+  // tslint:disable-next-line: typedef
   ngOnInit() {
   }
+  // tslint:disable-next-line: typedef
   login()
   {
    this.authService.login(this.model).subscribe(next => {
     this.alertify.success('logged in successfully');
    }, error => {
     this.alertify.error(error.error);
+   // tslint:disable-next-line: no-unused-expression
+   }, () => {
+     this.router.navigate(['/members']);
    });
   }
-
+  // tslint:disable-next-line: typedef
   loggedIn() {
     return this.authService.loggedIn();
   }
 
+  // tslint:disable-next-line: typedef
   logout()
   {
     localStorage.removeItem('token');
     this.alertify.error('logged Out');
+    this.router.navigate(['/home']);
+
   }
 
 }
